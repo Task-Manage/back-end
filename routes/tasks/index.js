4019dbb039e113a7670ecb69c5e0f29dff47fd84
Sarah Julia Gushef <sarahgushef@gmail.com>
2020-07-21 15:28:35 +0700

    Create get all tasks routes

--- routes/tasks/index.js
+++ routes/tasks/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { createTask, deleteTask } = require('./controller');
+const { createTask, deleteTask, getAllTasks } = require('./controller');
 const { verifyToken } = require('../../helpers');
 
+router.get('/', verifyToken, getAllTasks);
 router.post('/', verifyToken, createTask);
 router.delete('/:id', verifyToken, deleteTask);
 
