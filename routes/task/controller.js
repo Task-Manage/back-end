3a51763233f197bfbfdf516c0ab5071c26f7475d
sinta3 <sinta.tri38@gmail.com>
2020-07-21 11:15:32 +0700

    revisi task schema

--- routes/task/controller.js
+++ routes/task/controller.js
@@ -2,14 +2,11 @@ const { Task, User } = require('../../models');
 
 module.exports = {
     createTask: async (req, res) => {
-        const { id } = req.params;
-        const { assignment, status, reviews } = req.body;
+        const { assignment, status } = req.body;
         try {
             const result = await Task.create({
                 assignment,
-                status,
-                assignee: id,
-                reviews,
+                assignee,
             });
             res.send(result);
         } catch (error) {
